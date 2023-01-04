6b0c6e03fc29b09a231e1cc04dd1ac6ea4c2b6f6
Amber Oglesby <aoglesby5795@gmail.com>
2023-01-03 23:47:44 -0500

    updates

--- js/exercises.js
+++ js/exercises.js
@@ -12,6 +12,15 @@ function maxOfTwo(a, b){
    }
 
  maxOfTwo(2, 4)
+ //1. input is two numbers
+ //2. output is largest number
+ //3. determines the largest number
+ //4.defines largest number
+ //parameter (a,b)
+ // ask for max of two numbers
+ //return largest number either a or b
+ //5. expected result is 4
+ //7. run results: results are 4
 
   // ---------------------
   // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
@@ -26,18 +35,40 @@ function maxOfTwo(a, b){
    }
 
    maxOfThree(3,5,7)
-  // ---------------------
-  // Define a function isVowel() that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
-  // ---------------------
+
+   //1. input is three numbers(a,b,c)
+   //2. output is largest number (7)
+   //3. determines the largest number
+   //4.defines largest number
+   //parameter (a,b,c)
+   // ask for max of three numbers
+   //return largest number either a or b or c
+   //5. expected result is 7
+   //7. run results: results are 7
+   // ---------------------
+   // Define a function isVowel() that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
+   // ---------------------
   function isVowel(char){
     if char=='a' or char == 'e' or char == 'i' or char == 'o' or char == 'u'
   {return true;} 
    else  {return false;} 
 
-
+/* String.prototype.isVowel = function () {
+    var vowels = ['a', 'e', 'i', 'o', 'u'];
+    return vowels.indexOf(this.toLowerCase()) != -1; */
+};
   }
 
   isVowel(amber oglesby)
+   //1. input: NA
+   //2. output are vowels-true
+   //3. determines the vowels present
+   //4.returns vowels as true
+   //parameter (char)
+   // takes characters and retunr true if vowels
+   //return vowels as true
+   //5. expected result vowels as true and constanants as false
+   //7. results: SyntaxError: Unexpected identifier 'char'
 
   
 /* var YOURTEXTHERE = 'b';
@@ -54,13 +85,25 @@ else
   // Write a function rovarspraket() that will translate a text into "rövarspråket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
   // ---------------------
  
- /* def translate(str):
+ /* Answer found from countless google searches: 
+ def translate(str):
     consonants='bcdfghjklmnpqrstvwxyz'
     print("".join(l+'o'+l if l in consonants else l for l in str))
 
 string=input("Enter Any String: ")
 
 translate(string) */
+
+//1. input: any string
+   //2. output: double consonants
+   //3. doubles consonants
+   //4.doubles all present consonants
+   //parameter (str)
+   // doubles the present consonants
+   //
+   //5. expected results: doubled consonnts
+   //7. results:  SyntaxError: Unexpected identifier 'translate'
+
 
   // ---------------------
   // Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24.
